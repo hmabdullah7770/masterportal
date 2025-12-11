2d3433704a8aff1b359d98c51639b034ec9b735e
appdep <appdeployment64@gmail.com>
2025-12-11 14:06:49 +0500

    first commit

---
+++ master_portal_v1/src/app/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=" t-0 flex min-h-screen flex-col items-center justify-between p-24">
+        <h1> home 
+        about</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ master_portal_v1/src/app/user/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//localhost:3000/user
+const page = () => {
+  return (
+    <div>
+      hello use dive
+    </div>
+  )
+}
+
+export default page
